2572f96e6fd0e656518ca9705751cfd67f01745a
CodewithAndare-hub <139978681+CodewithAndare-hub@users.noreply.github.com>
2025-02-03 10:14:37 +0300

    Refactor MQTT message handling to parse JSON data and improve error handling

--- vending/mqtt_client.py
+++ vending/mqtt_client.py
@@ -7,24 +7,34 @@ topic = 'PLC_VENDING'
 
 
 # function to handle messages being posted
+import json
+
 def on_message(client, userdata, msg):
-    from .models import Machine, Transaction    
+    from .models import Machine, Transaction
     msg = msg.payload.decode('utf-8')
     print(msg)
-    if not msg == '':
+    if msg:
         try:
-            serial = msg['serial_no']
-            amount = msg['amount']
-            volume = msg['volume']
-            total_amount = msg['total_amount']
-            total_volume = msg['total_volume']
-            machine = Machine.objects.get(serial_number = serial)
-            if machine.exists() and machine.remaining_tokens > 1:
-                transaction = Transaction(machine = machine, amount = amount, volume = volume, total_amount = total_amount, total_volume = total_volume)
+            data = json.loads(msg)  # Parse the JSON string into a dictionary
+            serial = data['serial_no']
+            amount = data['amount']
+            volume = data['volume']
+            total_amount = data['total_amount']
+            total_volume = data['total_volume']
+            machine = Machine.objects.get(serial_number=serial)
+            if machine and machine.remaining_tokens > 1:
+                transaction = Transaction(
+                    machine=machine,
+                    amount=amount,
+                    volume=volume,
+                    total_amount=total_amount,
+                    total_volume=total_volume
+                )
                 transaction.save()
                 transaction.remaining_tokens()
         except Exception as e:
             print(e)
+
 def on_connnect(client, userdata, flags, rc, properties):
     if rc == 0:
         print("Connected to broker")
