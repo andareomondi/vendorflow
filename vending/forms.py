fb25d04872857a719793c1570c5bbbb50c0eeec7
CodewithAndare-hub <139978681+CodewithAndare-hub@users.noreply.github.com>
2025-02-08 09:19:44 +0300

    Update dashboard styling and enhance user registration form fields

--- vending/forms.py
+++ vending/forms.py
@@ -9,7 +9,7 @@ class UserRegistrationForm(forms.ModelForm):
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password']
+        fields = ['username','first_name', 'last_name' ,'email', 'password']
 
     def clean(self):
         cleaned_data = super().clean()
