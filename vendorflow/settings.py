8c0d602332a15e972e8121be1dbc17328eb94bd8
CodewithAndare-hub <139978681+CodewithAndare-hub@users.noreply.github.com>
2025-02-27 20:27:23 +0300

    set up cors because of spelling error

--- vendorflow/settings.py
+++ vendorflow/settings.py
@@ -21,7 +21,7 @@ load_dotenv()
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY= os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    'corheaders',
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
